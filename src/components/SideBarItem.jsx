041cf1a3cb5124f88ad0a787d156c2b1182b9b46
utsav-bhandari <145168913+utsav-bhandari@users.noreply.github.com>
2025-06-04 23:37:08 -0400

    sidbar refactor to work with details, plus other summary related css

--- src/components/SideBarItem.jsx
+++ src/components/SideBarItem.jsx
@@ -1,18 +1,22 @@
 function SideBarItem({ section }) {
     return (
         <li key={section.title}>
-            <a href={`#${section.title}`}>{section.title}</a>
-            {section.children.length > 0 && (
-                <ul className="sidebar-sublist">
-                    {/* Recursively render children using SideBarItem */}
-                    {section.children.map((childSection) => (
-                        <SideBarItem
-                            key={childSection.title}
-                            section={childSection}
-                        />
-                    ))}
-                </ul>
-            )}
+            <details>
+                <summary>
+                    <a href={`#${section.title}`}>{section.title}</a>
+                </summary>
+                {section.children.length > 0 && (
+                    <ul className="sidebar-sublist">
+                        {/* Recursively render children using SideBarItem */}
+                        {section.children.map((childSection) => (
+                            <SideBarItem
+                                key={childSection.title}
+                                section={childSection}
+                            />
+                        ))}
+                    </ul>
+                )}
+            </details>
         </li>
     );
 }
