b2995feb97914010df8587b3f3913f369c22d476
utsav-bhandari <145168913+utsav-bhandari@users.noreply.github.com>
2025-06-22 13:25:10 -0400

    removed duplicate links

--- src/components/Links/LinksList.jsx
+++ src/components/Links/LinksList.jsx
@@ -3,25 +3,33 @@ import { OutLink } from "../Svg";
 import { extractTitleFromWikiHref } from "../../lib/utils";
 
 function LinksList({ section, titles, onTitleClick }) {
+    const dupLinks = new Set();
     return (
         <ul>
-            {section.links.map((link, i) => (
-                <li key={i}>
-                    <a
-                        className="out-link"
-                        href={`${wikiUrl}${link.href}`}
-                        target="_blank"
-                        rel="noopener"
-                    >
-                        <OutLink />
-                    </a>
-                    <button onClick={() => onTitleClick(link.href)}>
-                        {link.text}{" "}
-                        {titles.has(extractTitleFromWikiHref(link.href)) &&
-                            "✅"}
-                    </button>
-                </li>
-            ))}
+            {section.links.map((link, i) => {
+                if (!dupLinks.has(link.text)) {
+                    dupLinks.add(link.text);
+                    return (
+                        <li key={i}>
+                            <a
+                                className="out-link"
+                                href={`${wikiUrl}${link.href}`}
+                                target="_blank"
+                                rel="noopener"
+                            >
+                                <OutLink />
+                            </a>
+                            <button onClick={() => onTitleClick(link.href)}>
+                                {link.text}{" "}
+                                {titles.has(
+                                    extractTitleFromWikiHref(link.href)
+                                ) && "✅"}
+                            </button>
+                        </li>
+                    );
+                }
+                return null;
+            })}
         </ul>
     );
 }
